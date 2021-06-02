6a8e63dc68b2281b1b8cf3ffd55467d1d920b24c
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2021-06-02 15:19:07 +0100

    genero done

--- js/views/generoVista.js
+++ js/views/generoVista.js
@@ -1,18 +1,26 @@
-// pegar em cada texto e trocar o innerHTML para o genero correto
+// pegar em cada texto e trocar o innerHTML para o genero correto// done
 
-import UserController from '../controllers/UserController.js'
+import utilizadorControlador from '../controllers/utilizadorControlador.js'
 
-const generoUtilizador = user.genero;
-let txtJogos = document.getElementById('textoJogos');
-let txtInicio = document.getElementById('textoInicio')
+export default class genero{
+    constructor(){
+        this.utilizadorControlador = new utilizadorControlador()
+        this.txtJogos = document.getElementById('textoJogos');
+        this.txtInicio = document.getElementById('textoInicio')
+    }
 
-if (generoUtilizador == 'feminino'){
-    txtJogos.innerHTML = 'Sê bem-vinda à área de jogos! Explora e diverte-te!';
-    txtInicio.innerHTML = 'Surgiu um bichinho perigoso chamado covid-19. Ele salta de pessoa em pessoa e deixa-as muito doentes. Os teus pais dizem-te que está no mundo inteiro, por isso, tens de ter muito cuidado. Estás preocupada e assustada? Comigo e com os meus amigos, tu estás segura.'
-}else if(generoUtilizador == 'masculino'){
-    txtJogos.innerHTML = 'Sê bem-vindo à área de jogos! Explora e diverte-te!';
-    txtInicio.innerHTML = 'Surgiu um bichinho perigoso chamado covid-19. Ele salta de pessoa em pessoa e deixa-as muito doentes. Os teus pais dizem-te que está no mundo inteiro, por isso, tens de ter muito cuidado. Estás preocupado e assustado? Comigo e com os meus amigos, tu estás seguro.'
-}else{
-    txtJogos.innerHTML = 'Sê bem-vinde à área de jogos! Explora e diverte-te!';
-    txtInicio.innerHTML = 'Surgiu um bichinho perigoso chamado covid-19. Ele salta de pessoa em pessoa e deixa-as muito doentes. Os teus pais dizem-te que está no mundo inteiro, por isso, tens de ter muito cuidado. Estás preocupade e assustade? Comigo e com os meus amigos, tu estás segure.'
+    textoGenero(){
+        const generoUtilizador = this.utilizadorControlador.getGenero();
+    
+        if (generoUtilizador == 'feminino'){
+            txtJogos.innerHTML = 'Sê bem-vinda à área de jogos! Explora e diverte-te!';
+            txtInicio.innerHTML = 'Surgiu um bichinho perigoso chamado covid-19. Ele salta de pessoa em pessoa e deixa-as muito doentes. Os teus pais dizem-te que está no mundo inteiro, por isso, tens de ter muito cuidado. Estás preocupada e assustada? Comigo e com os meus amigos, tu estás segura.'
+        }else if(generoUtilizador == 'masculino'){
+            txtJogos.innerHTML = 'Sê bem-vindo à área de jogos! Explora e diverte-te!';
+            txtInicio.innerHTML = 'Surgiu um bichinho perigoso chamado covid-19. Ele salta de pessoa em pessoa e deixa-as muito doentes. Os teus pais dizem-te que está no mundo inteiro, por isso, tens de ter muito cuidado. Estás preocupado e assustado? Comigo e com os meus amigos, tu estás seguro.'
+        }else{
+            txtJogos.innerHTML = 'Sê bem-vinde à área de jogos! Explora e diverte-te!';
+            txtInicio.innerHTML = 'Surgiu um bichinho perigoso chamado covid-19. Ele salta de pessoa em pessoa e deixa-as muito doentes. Os teus pais dizem-te que está no mundo inteiro, por isso, tens de ter muito cuidado. Estás preocupade e assustade? Comigo e com os meus amigos, tu estás segure.'
+        }  
+    }
 }
