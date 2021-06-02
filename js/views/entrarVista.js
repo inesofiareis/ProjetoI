2d70cc17f8bf9e5a34409d54c27fa92a26dcee97
Paulo Rodrigues <paulofilipe_100pt@hotmail.com>
2021-06-02 19:35:37 +0100

    mini ajustes

--- js/views/entrarVista.js
+++ js/views/entrarVista.js
@@ -15,21 +15,23 @@ export default class UserView {
     }
 
     verificarLogin() {
-        this.loginBotao.addEventListener('click', () => {
+        this.loginBotao.addEventListener('click', event => {
+
             try {
                 this.utilizadorControlador.login(this.utilizadorLogin.value, this.passwordLogin.value);
-                this.displayMessage('Login efetuado com sucesso!', 'success');
-
+                alert('Login efecutado com sucesso')
                 // Wait 1 second before reloading, so the user can see the login success message
-                setTimeout(() => {
-                    this.updateButtons('login');
-                    location.reload()
-                },
-                    1000);
+                // setTimeout(() => {
+                //     this.updateButtons('login');
+                //     location.reload()
+                // },
+                //     1000);
 
-            } catch (e) {
+            } 
+            catch (e) {
                 this.displayMessage(e, 'danger');
             }
+            event.preventDefault()
         });
 
         // this.logoutButton.addEventListener('click', () => {
@@ -53,5 +55,6 @@ export default class UserView {
     // displayMessage(message, type) {
     //     this.messages.innerHTML =
     //         `<div class="alert alert-${type}" role="alert">${message}</div>`;
+    //     alert(messages)
     // }
 }
