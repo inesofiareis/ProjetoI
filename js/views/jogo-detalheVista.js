bc4a75821bd1027d3c47ba403c698b07503b09b6
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2021-06-09 23:47:19 +0100

    Seleção de opçoes nos quizzes

--- js/views/jogo-detalheVista.js
+++ js/views/jogo-detalheVista.js
@@ -5,6 +5,11 @@ export default class JogosDetalhes {
         this.jogoControlador = new jogoControlador()
 
         this.posJogo = document.querySelector(".caixaJogos");
+        this.escolhaUm = document.querySelectorAll('input[name = "respostaUm"]');
+        this.escolhaDois = document.querySelectorAll('input[name = "respostaDois"]');
+        this.escolhaTres = document.querySelectorAll('input[name = "respostaTres"]');
+        this.escolhaQuatro = document.querySelectorAll('input[name = "respostaQuatro"]');
+        this.escolhaCinco = document.querySelectorAll('input[name = "respostaCinco"]');
         this.jogo()   
 
 
@@ -25,36 +30,63 @@ export default class JogosDetalhes {
 
         this.posJogo.innerHTML = `<h1 class="display-4">Vê o que aprendeste!</h1>
         <h3 class="perguntaUm">${jogo.perguntasErespostas[0].perguntas[0]}</h3>
-        <input type="radio" name="questaoUm" id="corretaUm" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[0][0]}<br>
-        <input type="radio" name="questaoUm" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[0][1]}<br>
-        <input type="radio" name="questaoUm" class="opcaoTres">${jogo.perguntasErespostas[0].alternativas[0][2]}<br>
-        <input type="radio" name="questaoUm" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[0][3]}<br>
+        <input type="radio" name="respostaUm" id="corretaUm" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[0][0]}<br>
+        <input type="radio" name="respostaUm" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[0][1]}<br>
+        <input type="radio" name="respostaUm" class="opcaoTres">${jogo.perguntasErespostas[0].alternativas[0][2]}<br>
+        <input type="radio" name="respostaUm" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[0][3]}<br>
     
         <h3 class="perguntaDois">${jogo.perguntasErespostas[0].perguntas[1]}</h3>
-        <input type="radio" name="questaoDois" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[1][0]} <br>
-        <input type="radio" name="questaoDois" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[1][1]} <br>
-        <input type="radio" name="questaoDois" id="corretaDois" class="opcaoTres">${jogo.perguntasErespostas[1].alternativas[0][2]} <br>
-        <input type="radio" name="questaoDois" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[1][3]} <br>
+        <input type="radio" name="respostaDois" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[1][0]} <br>
+        <input type="radio" name="respostaDois" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[1][1]} <br>
+        <input type="radio" name="respostaDois" id="corretaDois" class="opcaoTres">${jogo.perguntasErespostas[1].alternativas[0][2]} <br>
+        <input type="radio" name="respostaDois" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[1][3]} <br>
     
         <h3 class="perguntaTres">${jogo.perguntasErespostas[0].perguntas[2]}</h3>
-        <input type="radio" name="question3" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[2][0]} <br>
-        <input type="radio" name="question3" id="corretaDois" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[2][1]} <br>
-        <input type="radio" name="question3" class="opcaoTres">${jogo.perguntasErespostas[0].alternativas[2][2]} <br>
-        <input type="radio" name="question3" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[0][3]} <br>
+        <input type="radio" name="respostaTres" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[2][0]} <br>
+        <input type="radio" name="respostaTres" id="corretaDois" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[2][1]} <br>
+        <input type="radio" name="respostaTres" class="opcaoTres">${jogo.perguntasErespostas[0].alternativas[2][2]} <br>
+        <input type="radio" name="respostaTres" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[0][3]} <br>
     
         <h3 class="perguntaQuatro">${jogo.perguntasErespostas[0].perguntas[3]}</h3>
-        <input type="radio" name="question3" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[3][0]} <br>
-        <input type="radio" name="question3" id="corretaTres" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[0][1]} <br>
-        <input type="radio" name="question3" class="opcaoTres">${jogo.perguntasErespostas[0].alternativas[3][2]} <br>
-        <input type="radio" name="question3" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[3][3]} <br>
+        <input type="radio" name="respostaQuatro" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[3][0]} <br>
+        <input type="radio" name="respostaQuatro" id="corretaTres" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[0][1]} <br>
+        <input type="radio" name="respostaQuatro" class="opcaoTres">${jogo.perguntasErespostas[0].alternativas[3][2]} <br>
+        <input type="radio" name="respostaQuatro" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[3][3]} <br>
     
         <h3 class="perguntaCinco">${jogo.perguntasErespostas[0].perguntas[4]}</h3>
-        <input type="radio" name="question3" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[4][0]} <br>
-        <input type="radio" name="question3" id="corretaQuatro" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[4][1]} <br>
-        <input type="radio" name="question3" class="opcaoTres">${jogo.perguntasErespostas[0].alternativas[4][2]} <br>
-        <input type="radio" name="question3" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[4][3]} <br>
+        <input type="radio" name="respostaCinco" class="opcaoUm">${jogo.perguntasErespostas[0].alternativas[4][0]} <br>
+        <input type="radio" name="respostaCinco" id="corretaQuatro" class="opcaoDois">${jogo.perguntasErespostas[0].alternativas[4][1]} <br>
+        <input type="radio" name="respostaCinco" class="opcaoTres">${jogo.perguntasErespostas[0].alternativas[4][2]} <br>
+        <input type="radio" name="respostaCinco" class="opcaoQuatro">${jogo.perguntasErespostas[0].alternativas[4][3]} <br>
     
-        <input type="submit" name="submit" value="Diz-me a pontuação!" onClick="resultado()">`
+        <input type="submit" name="submit" value="Diz-me a pontuação!" id="resultado">`
+
+        this.botaoResultado = document.querySelector('#resultado').addEventListener('click', function(){
+            for (let i = 0; i < 4; i++){
+                if (this.escolhaUm[i].checked){
+                    let rUm = this.escolhaUm[i].checked
+                }
+            
+                if (this.escolhaDois[i].checked){
+                    let rDois = this.escolhaDois[i].checked
+                }
+            
+                if (this.escolhaTres[i].checked){
+                    let rTres = this.escolhaTres[i].checked
+                }
+            
+                if (this.escolhaQuatro[i].checked){
+                    let rQuatro = this.escolhaQuatro[i].checked
+                }
+        
+                if (this.escolhaCinco[i].checked){
+                    let rCinco = this.escolhaCinco[i].checked
+                }
+            }
+
+            this.utilizadorControlador.respostaDada(rUm, rDois, rTres, rQuatro, rCinco)
+
+        })
     }
 
     jogo() {
