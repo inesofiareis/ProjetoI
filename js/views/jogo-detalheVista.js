91ff65f66a5dc12e0834746cc45c7a23d2ed3439
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2021-06-10 02:11:09 +0100

    Segundo jogo adicionado e em progresso

--- js/views/jogo-detalheVista.js
+++ js/views/jogo-detalheVista.js
@@ -89,6 +89,97 @@ export default class JogosDetalhes {
         })
     }
 
+    gerarJogoArrastar(){
+        this.posJogo.innerHTML = `<div class="jogoDeArrastar">
+        <section class="elementosArrastar">
+            <img class="arastar" draggable="true" src="" id="virusdrop">
+            <img class="arastar" draggable="true" src="" id="mascaradrop">
+            <img class="arastar" draggable="true" src="" id="alcooldrop">
+            <img class="arastar" draggable="true" src="" id="distanciamentodrop">
+            <img class="arastar" draggable="true" src="" id="lavarAsMaosdrop">
+            <img class="arastar" draggable="true" src="" id="doencadrop">
+        </section>
+        <section class="elementoLargar">
+            <div class="largar" data-id="virusdrop"><span>Vírus</span>
+            </div>
+            <div class="largar" data-id="mascaradrop">
+                <span>Máscara</span>
+            </div>
+            <div class="largar" data-id="alcooldrop">
+                <span>Álcool-gel</span>
+            </div>
+            <div class="largar" data-id="distanciamentodrop">
+                <span>Distanciamento</span>
+            </div>
+            <div class="largar" data-id="lavarAsMaosdrop">
+                <span>Lavar as mãos</span>
+            </div>
+            <div class="largar" data-id="doencadrop">
+                <span>Doença</span>
+            </div>
+        </section>
+        </div>`
+
+        const elementosArrastar = document.querySelectorAll('.arrastar');
+        const elementosLargar = document.querySelectorAll('.largar');
+
+        elementosArrastar.forEach(elem => {
+            elem.addEventListener('arrastarInicio', arrastarInicio);
+            elem.addEventListener("arrastar", arrastar);
+            elem.addEventListener("arrastarFim",arrastarFim);
+
+        })
+
+        elementosLargar.forEach(elem => {
+            elem.addEventListener('largarEntrada',largarEntrada);
+            elem.addEventListener('largarValido',largarValido);
+            elem.addEventListener('largarFim',largarFim);
+            elem.addEventListener('largar',largar);
+        })
+        // controller mas sem certezas 
+
+        // function arrastarInicio(event){
+        //     event.dataTransfer.setData("text", event.target.id);
+        // }
+
+        // function largarEntrada(event) {
+        //     if(!event.target.classList.contains("dropped")) {
+        //       event.target.classList.add("droppable-hover");
+        //     }
+        //   }
+          
+        //   function largarValido(event) {
+        //     if(!event.target.classList.contains("dropped")) {
+        //       event.preventDefault(); // Prevent default to allow drop
+        //     }
+        //   }
+          
+        //   function largarFim(event) {
+        //     if(!event.target.classList.contains("dropped")) {
+        //       event.target.classList.remove("droppable-hover");
+        //     }
+        //   }
+          
+        //   function largar(event) {
+        //     event.preventDefault(); // This is in order to prevent the browser default handling of the data
+        //     event.target.classList.remove("droppable-hover");
+        //     const elementosArrastarData = event.dataTransfer.getData("text"); // Get the dragged data. This method will return any data that was set to the same type in the setData() method
+        //     const elementosLargarData = event.target.getAttribute("data-draggable-id");
+        //     const respostaCorreta = elementosArrastarData === elementosLargarData;
+        //     if(respostaCorreta) {
+        //       const elementoArrastar = document.getElementById(elementosArrastarData);
+        //       event.target.classList.add("dropped");
+        //       // event.target.style.backgroundColor = draggableElement.style.color; // This approach works only for inline styles. A more general approach would be the following: 
+        //       event.target.style.backgroundColor = window.getComputedStyle(elementoArrastar).color;
+        //       elementoArrastar.classList.add("dragged");
+        //       elementoArrastar.setAttribute("draggable", "false");
+        //       event.target.insertAdjacentHTML("afterbegin", `<i class="fas fa-${elementoArrastar}"></i>`);
+        //     }
+        //   }
+
+
+    }
+
     jogo() {
         let jogo = this.jogoControlador.getJogoAtual();
         if(jogo.genero == 'Preencher espaços') {
