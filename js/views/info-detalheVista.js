e083cd963e9764d529ec16ed41b3c3da75fa5e89
Inês Reis <40200335@esmad.ipp.pt>
2021-06-15 17:39:10 +0100

    alteração de detalhes

--- js/views/info-detalheVista.js
+++ js/views/info-detalheVista.js
@@ -18,9 +18,9 @@ export default class infoDetelhes {
     }
 
     gerarTexto(info){
-        let resultado = `<div class="introducaoInfo">
-                            <h1 class="text-center tituloInfo">${info.titulo}</h1>
-                            <p class="text-center conteudoInfo">${info.subtitulo}</p>
+        let resultado = `<div class="introducaoInfo" style="padding-top: 20px;">
+                            <h1 class="text-center tituloInfo" style="font-size: 50px; color:#205D76; ">${info.titulo}</h1>
+                            <p class="text-center conteudoInfo" style="font-size: 30px; color:#3F9999;">${info.subtitulo}</p>
                             <img class="img-fluid imgInfo" src="${info.imagem}" />
                         </div>`
 
@@ -34,7 +34,7 @@ export default class infoDetelhes {
     gerarParagrados(info){
         let paragrafo = `<div class="text">`
         for (const texto of info.texto){
-            paragrafo += `<p class="paragrafo">${texto}</p>`
+            paragrafo += `<p class="paragrafo text-justify" style="font-size: 21px; padding-top:10px; color:#2B4E5C;">${texto}</p>`
         }
 
         paragrafo += `</div>`
